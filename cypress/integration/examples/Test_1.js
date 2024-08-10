d7faab35420e30ccf3728d3f87332c37604834ab
ardra_ashok <ardraasok@yahoo.in>
2024-08-10 17:13:10 +1000

    Added basics  cypress testing, function in command.js and data in example.json

--- cypress/integration/examples/Test_1.js
+++ cypress/integration/examples/Test_1.js
@@ -1,6 +1,28 @@
+
+
 describe('Calender Tests', function () {
- it('Verify date selection', function () {
-  cy.visit(Cypress.env('url') + '/angularpractice')
-  
- })
- })
+  before(function () {
+    cy.fixture('example').then(function (data) {
+      this.data = data
+    })
+  })
+  it('Verify date selection', function () {
+    cy.visit(Cypress.env('url') + '/angularpractice/')
+    cy.get('form input[name*="name"]').type(this.data.name)
+    cy.get('form input[name*="email"]').type(this.data.email)
+    cy.get('select').select(this.data.gender)
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name) // can also be done by .text() method
+    cy.get('form input[name*="name"]').should('have.attr', 'minlength', '2') // can also be done by prop method
+    cy.get('#inlineRadio3').should('be.disabled')
+    cy.pause()
+    cy.get(':nth-child(2) > .nav-link').click()
+    // cy.get(':nth-child(2) > .nav-link').click().debug()
+
+    // cy.selectProduct(this.data.product_name)
+
+    this.data.product_names.forEach((prod) => {
+      cy.log(prod)
+      cy.selectProduct(prod)
+    })
+  })
+})
