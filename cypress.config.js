049554bcd64d213466995e5d356feac48e5966b0
ardra_ashok <ardraasok@yahoo.in>
2024-08-10 23:51:59 +1000

    Added functionality for reporting using mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout:6000,
+  projectId: 'wstg6o',
+  defaultCommandTimeout: 6000,
+  reporter: 'cypress-mochawesome-reporter',
   env: {
     url: 'https://rahulshettyacademy.com',
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
     specPattern: 'cypress/integration/examples/*.js',
   },
